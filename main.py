a867912d1f08277127e72a8497d9b35ab36da3e0
Denilsonaraujoprogramador <101232538+Denilsonaraujoprogramador@users.noreply.github.com>
2022-07-16 13:10:46 -0300

    Criando as funções (def)
    Criando as funções

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 
 # View - informação que o usuário irá solicitar.
-usuario_digitado = input("Informa o seu usuário: ")
-senha_digitado = input("informa a sua senha: ")
+def view():
+    usuario_digitado = input("Informa o seu usuário: ")
+    senha_digitado = input("informa a sua senha: ")
+    controller(usuario_digitado, senha_digitado)
 
-# Model - Infomação que virá da meu Banco de dados(BD) uma request do tipo GET(requisição).
-usuario_BD = 'joao'
-senha_BD = '123'
+# Model - Infomação que virá da meu Banco de dados(BD) através de uma request do tipo GET(requisição).
+def model_usuario():
+    usuario_BD = 'joao'
+    return usuario_BD
+
+def model_senha():
+    senha_BD = '123'
+    return senha_BD
 
 # Controller - irá relalizar a validação a informação solicitada pelo usuário e oretorno do Banco de dados.
-if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
-    print('pode entrar')
-else:
-    print('usuário ou senha inválidos')
+def controller(usuario_digitado, senha_digitado):
+    usuario_BD = model_usuario
+    senha_BD = model_senha
+    if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
+        print('pode entrar')
+    else:
+        print('usuário ou senha inválidos')
+
+view()
